4e023abac7ece9295f09280562b48a575c1dd88a
Karina Pichardo <pichardokarina21@yahoo.com>
2020-04-23 14:34:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number. */
+/* Write a function that accepts an array of 10 integers (between 0 and 9), 
+that returns a string of those numbers in the form of a phone number. */
 
 // 1 input - array of numbers 
 function createPhoneNumber(numbers){
